1d83cedad2c642592d3b06d25c35cde72402827e
Mark Calvelo <combatdstar@gmail.com>
2019-09-04 10:36:18 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 // declare your functions here...
+// $('selector-goes-here')
+// $('img') element selector
+// $('.pics') class selector
+// $('#baby-ninja') id selector
+
+// $('ul li') select all lis in the ul
+
+// $('parent-tag child-tag:first-child')
+// $('div img:first-child');
+
+// $("img[alt='the beatles making faces']");
+// $('div:last');
+
+function paragraphSelector() {
+  $('p');
+}
